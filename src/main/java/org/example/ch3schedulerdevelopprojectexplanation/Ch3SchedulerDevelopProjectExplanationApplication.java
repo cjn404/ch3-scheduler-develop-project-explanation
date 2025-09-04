1bd6681d5678e451640a3b4d13f604ae744fe463
cjn404 <moi404@naver.com>
2025-09-04 09:42:08 +0900

    first commit

---
+++ src/main/java/org/example/ch3schedulerdevelopprojectexplanation/Ch3SchedulerDevelopProjectExplanationApplication.java
@@ -0,0 +1,13 @@
+package org.example.ch3schedulerdevelopprojectexplanation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch3SchedulerDevelopProjectExplanationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch3SchedulerDevelopProjectExplanationApplication.class, args);
+    }
+
+}
