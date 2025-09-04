343860d4429b75ae57bb7563fffb2ec24ce05b70
cjn404 <moi404@naver.com>
2025-09-04 10:54:40 +0900

    [1st] feat: UserDto 구현

--- src/main/java/org/example/ch3schedulerdevelopprojectexplanation/user/dto/response/UserResponseDto.java
+++ src/main/java/org/example/ch3schedulerdevelopprojectexplanation/user/dto/response/UserResponseDto.java
@@ -1,4 +1,24 @@
 package org.example.ch3schedulerdevelopprojectexplanation.user.dto.response;
 
+import lombok.Getter;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class UserResponseDto {
+
+    private final Long id;
+    private final String userName;
+    private final String email;
+    private final LocalDateTime createdAt;
+    private final LocalDateTime updatedAt;
+
+    public UserResponseDto(Long id, String userName, String email, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.userName = userName;
+        this.email = email;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
 }
