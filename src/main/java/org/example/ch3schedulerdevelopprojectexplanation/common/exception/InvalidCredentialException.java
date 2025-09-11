31d9f72f925e459c34c52b8666b394cfcc3e21c0
cjn404 <moi404@naver.com>
2025-09-12 00:25:50 +0900

    [1st] feat: 커스텀 예외 클래스 구현

---
+++ src/main/java/org/example/ch3schedulerdevelopprojectexplanation/common/exception/InvalidCredentialException.java
@@ -0,0 +1,10 @@
+package org.example.ch3schedulerdevelopprojectexplanation.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class InvalidCredentialException extends ApplicationException {
+
+    public InvalidCredentialException(String message) {
+        super(message, HttpStatus.UNAUTHORIZED);
+    }
+}
