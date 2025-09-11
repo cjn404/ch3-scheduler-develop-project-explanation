074db0c360155dd2e020b32c75a193ceb78ab233
cjn404 <moi404@naver.com>
2025-09-11 22:50:34 +0900

    [1st] feat: User, BaseEntity 구현

--- src/main/java/org/example/ch3schedulerdevelopprojectexplanation/user/entity/User.java
+++ src/main/java/org/example/ch3schedulerdevelopprojectexplanation/user/entity/User.java
@@ -1,4 +1,34 @@
 package org.example.ch3schedulerdevelopprojectexplanation.user.entity;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.ch3schedulerdevelopprojectexplanation.common.entity.BaseEntity;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    public void update(
+            String userName,
+            String email,
+            String password
+    ) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
 }
