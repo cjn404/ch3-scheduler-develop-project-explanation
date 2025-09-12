7aa409765f1758ed898bc11ebffc576d45f17c03
cjn404 <moi404@naver.com>
2025-09-13 01:17:37 +0900

    [1st] feat: 로그인 필터 구현

---
+++ src/main/java/org/example/ch3schedulerdevelopprojectexplanation/common/filter/FilterConfig.java
@@ -0,0 +1,19 @@
+package org.example.ch3schedulerdevelopprojectexplanation.common.filter;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<Filter> loginFilter() {
+        FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LoginFilter());
+        registrationBean.setOrder(1);
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+}
