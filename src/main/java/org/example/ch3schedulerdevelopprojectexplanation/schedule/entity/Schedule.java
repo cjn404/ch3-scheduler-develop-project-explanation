73f8a41ef8c7a04e471589ffd65e5ed5ce76325c
cjn404 <moi404@naver.com>
2025-10-04 22:11:27 +0900

    [1st] feat: Schedule 엔티티 구현

---
+++ src/main/java/org/example/ch3schedulerdevelopprojectexplanation/schedule/entity/Schedule.java
@@ -0,0 +1,40 @@
+package org.example.ch3schedulerdevelopprojectexplanation.schedule.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.ch3schedulerdevelopprojectexplanation.common.entity.BaseEntity;
+import org.example.ch3schedulerdevelopprojectexplanation.user.entity.User;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "schedules")
+public class Schedule extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String title;
+    private String content;
+
+    public Schedule(
+            User user,
+            String title,
+            String content
+    ) {
+        this.user = user;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
