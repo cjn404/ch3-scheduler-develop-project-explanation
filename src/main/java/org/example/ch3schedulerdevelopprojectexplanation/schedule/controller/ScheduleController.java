cc3d1a9d6b8a151b552d97ba2ee4340342c8bcf8
cjn404 <moi404@naver.com>
2025-10-05 21:40:22 +0900

    [1st] feat: Schedule findAll 메서드 구현

--- src/main/java/org/example/ch3schedulerdevelopprojectexplanation/schedule/controller/ScheduleController.java
+++ src/main/java/org/example/ch3schedulerdevelopprojectexplanation/schedule/controller/ScheduleController.java
@@ -3,25 +3,31 @@ package org.example.ch3schedulerdevelopprojectexplanation.schedule.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.ch3schedulerdevelopprojectexplanation.common.consts.Const;
 import org.example.ch3schedulerdevelopprojectexplanation.schedule.dto.request.ScheduleSaveRequestDto;
+import org.example.ch3schedulerdevelopprojectexplanation.schedule.dto.response.ScheduleResponseDto;
 import org.example.ch3schedulerdevelopprojectexplanation.schedule.dto.response.ScheduleSaveResponseDto;
 import org.example.ch3schedulerdevelopprojectexplanation.schedule.service.ScheduleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/schedules")
 public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
-    @PostMapping("/schedules")
+    @PostMapping
     public ResponseEntity<ScheduleSaveResponseDto> save(
             @SessionAttribute(name = Const.LOGIN_USER) Long userId,
             @RequestBody ScheduleSaveRequestDto dto
     ) {
         return ResponseEntity.ok(scheduleService.save(userId, dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ScheduleResponseDto>> findAll() {
+        return ResponseEntity.ok(scheduleService.findAll());
     }
 }
--- src/main/java/org/example/ch3schedulerdevelopprojectexplanation/schedule/service/ScheduleService.java
+++ src/main/java/org/example/ch3schedulerdevelopprojectexplanation/schedule/service/ScheduleService.java
@@ -2,12 +2,16 @@ package org.example.ch3schedulerdevelopprojectexplanation.schedule.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.ch3schedulerdevelopprojectexplanation.schedule.dto.request.ScheduleSaveRequestDto;
+import org.example.ch3schedulerdevelopprojectexplanation.schedule.dto.response.ScheduleResponseDto;
 import org.example.ch3schedulerdevelopprojectexplanation.schedule.dto.response.ScheduleSaveResponseDto;
 import org.example.ch3schedulerdevelopprojectexplanation.schedule.entity.Schedule;
 import org.example.ch3schedulerdevelopprojectexplanation.schedule.repository.ScheduleRepository;
 import org.example.ch3schedulerdevelopprojectexplanation.user.entity.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -32,5 +36,24 @@ public class ScheduleService {
                 schedule.getCreatedAt(),
                 schedule.getUpdatedAt()
         );
+    }
+
+    @Transactional(readOnly = true)
+    public List<ScheduleResponseDto> findAll() {
+        List<Schedule> schedules = scheduleRepository.findAll();
+        List<ScheduleResponseDto> dtos = new ArrayList<>();
+
+        for (Schedule schedule : schedules) {
+            ScheduleResponseDto dto = new ScheduleResponseDto(
+                    schedule.getId(),
+                    schedule.getUser().getId(),
+                    schedule.getTitle(),
+                    schedule.getContent(),
+                    schedule.getCreatedAt(),
+                    schedule.getUpdatedAt()
+            );
+            dtos.add(dto);
+        }
+        return dtos;
     }
 }
