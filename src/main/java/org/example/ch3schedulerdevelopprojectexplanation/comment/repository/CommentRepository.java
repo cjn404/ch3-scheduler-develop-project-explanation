a7d2675792752450fd0ff704e8395e02bcb1e961
cjn404 <moi404@naver.com>
2025-10-06 19:53:28 +0900

    [1st] feat: Comment 레포지토리 구현

---
+++ src/main/java/org/example/ch3schedulerdevelopprojectexplanation/comment/repository/CommentRepository.java
@@ -0,0 +1,19 @@
+package org.example.ch3schedulerdevelopprojectexplanation.comment.repository;
+
+import org.example.ch3schedulerdevelopprojectexplanation.comment.dto.CommentCountDto;
+import org.example.ch3schedulerdevelopprojectexplanation.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @Query("""
+            SELECT new org.example.ch3schedulerdevelopprojectexplanation.comment.dto.CommentCountDto(c.schedule.id, count(c))
+            FROM Comment c
+            WHERE c.schedule.id IN :scheduleIds
+            GROUP BY c.schedule.id
+            """)
+    List<CommentCountDto> countByScheduleIds(List<Long> scheduleIds);
+}
